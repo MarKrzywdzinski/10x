78af69636f697e9762d63f60544d363205f49214
MarKrzywdzinski <36741633+MarKrzywdzinski@users.noreply.github.com>
2025-07-10 22:53:24 +0200

    Dalsza konfiguracja

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://10xcards.pl",
-  integrations: [react(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
   output: "server",
-  adapter: cloudflare({
-    mode: "directory",
-    functionPerRoute: true,
-  }),
-  experimental: {
-    session: true,
-  },
+  adapter: cloudflare(),
+  integrations: [
+    react({
+      include: ["**/components/**/*.tsx"],
+    }),
+    tailwind(),
+  ],
 });
