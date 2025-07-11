221e7e6c88c5abe5a84ebda280dcfb857c03b9ed
MarKrzywdzinski <36741633+MarKrzywdzinski@users.noreply.github.com>
2025-07-11 17:58:53 +0200

    kolejna proba deployu

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
+  // Usuwamy adapter cloudflare, bo nie jest potrzebny dla statycznego outputu
+  // adapter: cloudflare(),
   integrations: [
     react({
       include: ["**/components/**/*.tsx"],
