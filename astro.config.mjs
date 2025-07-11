9bb1a1f27be806c7c2591b3237757a17385b9ad0
MarKrzywdzinski <36741633+MarKrzywdzinski@users.noreply.github.com>
2025-07-11 17:35:57 +0200

    Zmiany w astro i master.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: cloudflare(),
   integrations: [
     react({
